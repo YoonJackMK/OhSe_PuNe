44df3eb805d2f4820e4c376ae5e6fc2620403deb
YoonMK <gentleymk@naver.com>
2016-11-08 13:49:58 +0900

    mk

---
+++ OhSe_PuNe/src/game_ohse/test.java
@@ -0,0 +1,5 @@
+package game_ohse;
+
+public class test {
+
+}
---
+++ OhSe_PuNe/src/server/SV_Test.java
@@ -0,0 +1,10 @@
+package server;
+
+public class SV_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
